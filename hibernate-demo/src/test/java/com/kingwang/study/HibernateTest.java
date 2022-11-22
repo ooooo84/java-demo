50adfc25d6a5ad5586d14c01d33b4060523acd38
King <badly1984@hotmail.com>
2022-11-22 13:49:01 +0800

    Add hibernate cascade save example

--- hibernate-demo/src/test/java/com/kingwang/study/HibernateTest.java
+++ hibernate-demo/src/test/java/com/kingwang/study/HibernateTest.java
@@ -1,6 +1,7 @@
 package com.kingwang.study;
 
 import com.kingwang.study.entity.Customer;
+import com.kingwang.study.entity.CustomerOrder;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -10,6 +11,17 @@ import java.util.UUID;
 
 public class HibernateTest {
     public static void main(String[] args) {
+//        basicUsage();
+
+        SessionFactory sessionFactory = createSessionFactory();
+
+        cascadeSave(sessionFactory);
+    }
+
+    /**
+     * 最简易使用示例
+     */
+    private static void basicUsage() {
         // 1. 创建Configuration对象
         Configuration configuration = new Configuration().configure();
 
@@ -20,13 +32,44 @@ public class HibernateTest {
         try (Session session = sessionFactory.openSession()) {
             Customer newCustomer = new Customer();
             newCustomer.setId(UUID.randomUUID().toString());
-            newCustomer.setName("张三");
+            newCustomer.setName("李四");
 
             session.save(newCustomer);
             Transaction transaction = session.beginTransaction();
             transaction.commit();
         }
 
-        System.out.println(configuration);
+        System.out.println("添加Customer完成");
+    }
+
+    /**
+     * 级联保存的使用
+     *
+     * @param sessionFactory
+     */
+    private static void cascadeSave(SessionFactory sessionFactory) {
+        try (Session session = sessionFactory.openSession()) {
+            Customer newCustomer = new Customer();
+            newCustomer.setId(UUID.randomUUID().toString());
+            newCustomer.setName("王五");
+
+            CustomerOrder order = new CustomerOrder();
+            order.setId(UUID.randomUUID().toString());
+            order.setName("订单1");
+            order.setCustomer(newCustomer);
+
+            session.save(newCustomer);
+            session.save(order);
+
+            session.beginTransaction().commit();
+        }
+    }
+
+    private static SessionFactory createSessionFactory() {
+        // 1. 创建Configuration对象
+        Configuration configuration = new Configuration().configure();
+
+        // 2. 获取SessionFactory
+        return configuration.buildSessionFactory();
     }
 }
