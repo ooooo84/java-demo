5df8f12e6cd605efd2321269e0302dd7eb100c48
King <badly1984@hotmail.com>
2022-11-22 16:13:30 +0800

    Rename file

--- hibernate-demo/src/test/java/com/kingwang/study/HibernateLazyLoadingTest.java
+++ hibernate-demo/src/test/java/com/kingwang/study/HibernateLazyLoadingTest.java
@@ -10,7 +10,7 @@ import org.hibernate.SessionFactory;
 
 import java.util.Set;
 
-public class HibernateQueryTest {
+public class HibernateLazyLoadingTest {
     public static void main(String[] args) {
         SessionFactory sessionFactory = HibernateUtil.createSessionFactory();
 
