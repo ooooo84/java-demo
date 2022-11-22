a29a7e74e63526a563aa171d5427fce83ff367a2
King <badly1984@hotmail.com>
2022-11-22 17:02:43 +0800

    Refactoring hibernate SessionFactory init code to 5.x

--- hibernate-demo/src/main/java/com/kingwang/study/util/HibernateUtil.java
+++ hibernate-demo/src/main/java/com/kingwang/study/util/HibernateUtil.java
@@ -1,12 +1,20 @@
 package com.kingwang.study.util;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public final class HibernateUtil {
     public static SessionFactory createSessionFactory() {
-        Configuration configuration = new Configuration().configure();
+        // 以下为Hibernate 3/4版本的写法
+//        Configuration configuration = new Configuration().configure();
+//
+//        return configuration.buildSessionFactory();
 
-        return configuration.buildSessionFactory();
+        // Hibernate 5.x版本写法
+        StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().configure().build();
+
+        return new MetadataSources(serviceRegistry).buildMetadata().buildSessionFactory();
     }
 }
